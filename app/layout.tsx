08b2a083e622bad62f39fb4fbe042c4f4fb67f3b
housenafis07-ctrl <housenafis07@gmail.com>
2026-09-23 11:44:45 +0500

    fix: keep safe one-shot Royalhouse branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import HomeBuildingNavigationFix from "./components/HomeBuildingNavigationFix";
 import MobileMainNavigation from "./components/MobileMainNavigation";
 import PushRegistration from "./components/PushRegistration";
 import ListingCardIconStyle from "./components/ListingCardIconStyle";
+import RoyalhouseBrandFix from "./components/RoyalhouseBrandFix";
 import RoyalhouseFooterFix from "./components/RoyalhouseFooterFix";
 import AuthLanguageFix from "./components/AuthLanguageFix";
 import RoyalhousePartnersLinkFix from "./components/RoyalhousePartnersLinkFix";
@@ -27,6 +28,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="uz">
       <body>
         <I18nProvider>
+          <RoyalhouseBrandFix />
           <RoyalhouseFooterFix />
           <GlobalLanguageFix />
           <LegacySurfaceLanguageFix />
